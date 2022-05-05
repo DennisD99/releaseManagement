4a9eb856d33ae8c7094a815858dbfbb6e14099b6
Dennis Dusmetov <dennisdusmetov@DENNISS-MBP.station>
2022-05-05 12:07:22 +0100

    Commit for unit testing

---
+++ unittestingforCA.py
@@ -0,0 +1,40 @@
+import unittest
+
+import urllib.request
+
+
+class BasicTests(unittest.TestCase):
+
+
+
+    def test_main_page(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+   
+
+    def test_page2(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/page2").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+        
+    def test_page3(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/page3").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+   
+    def test_get_number(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/getNumber").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
